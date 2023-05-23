e1a3c69917d9328b57bee91ce0de02fc49db2ae8
Hamza <hamza.benali@yellow-it.fr>
2023-05-23 19:23:24 +0100

    NewProject/ first Commit

---
+++ src/tn/esprit/tests/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.tests;
+
+import tn.esprit.entities.Personne;
+import tn.esprit.services.PersonneService;
+
+/**
+ *
+ * @author Fayechi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PersonneService ps = new PersonneService();
+        Personne p = new Personne(1, 33, "Belkneni", "Maroua");
+        ps.ajouter(p);
+        
+        // ps.supprimer(p);
+        
+        //System.out.println(ps.afficher());;
+    }
+    
+}
---
+++ src/tn/esprit/tools/MaConnexion.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.tools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Fayechi
+ */
+public class MaConnexion {
+    String url ="jdbc:mysql://localhost:3306/javaapplication";
+    String user ="root";
+    String pwd="";
+     private Connection cnx;
+    public static MaConnexion ct;
+
+    private MaConnexion() {
+        try {
+            cnx = DriverManager.getConnection(url, user, pwd);
+            System.out.println("Cnx etablie !!");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public static MaConnexion getInstance(){
+        if(ct==null){
+            ct= new MaConnexion();
+        }
+        return ct;
+    }
+
+    public Connection getCnx() {
+        return cnx;
+    }
+    
+}
